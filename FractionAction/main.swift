4ff228c97e5dfcd93c8ab6c34a7559995492cae1
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2018-05-02 19:30:31 -0400

    Fraction action half way done

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -9,13 +9,75 @@
 import Foundation
 
 // INPUT
-// Collect and filter user input here
+
+// Numerator
+var validNumeratorInput = 0
+
+while 1 == 1 {
+    
+    print("Numerator?")
+ 
+// If it is nil
+    guard let numeratorInput = readLine() else {
+        continue
+    }
+    
+// If it is an interger
+    guard let numeratorNumber = Int(numeratorInput) else {
+        continue
+    }
+
+// If it is positive
+    if numeratorNumber < 0 {
+        continue
+    }
+
+// The input is correct
+ validNumeratorInput = numeratorNumber
+    break
+}
+
+// Denominator
+var validDenominatorInput = 0
+
+while 1 == 1 {
+    
+    print("Denominator?")
+    
+    // If it is nil
+    guard let denominatorInput = readLine() else {
+        continue
+    }
+    
+    // If it is an interger
+    guard let denominatorNumber = Int(denominatorInput) else {
+        continue
+    }
+    
+    // If it is 0
+    if denominatorNumber == 0 {
+        continue
+    }
+    
+    // The input is correct
+    validDenominatorInput = denominatorNumber
+    break
+}
 
 
 // PROCESS
-// Implement the primary logic of the problem here
+// The integer part of the fraction
+var integerResult = validNumeratorInput / validDenominatorInput
 
+// The remainder part of the fraction
+var remainderResult = validNumeratorInput % validDenominatorInput
+
+
+// The answer in total
+var finalAnswer = integerResult + remainderResult / 
 
 // OUTPUT
+print("The result is?")
+print(finalAnswer)
 // Report results to the user here
 
